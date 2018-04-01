4160b9c0e14c320ddb4a546c7ba80f9feee01ebb
Marco Crespi <mrc@flxlabs.com>
2018-04-01 22:16:36 +0200

    fix(mod-fake): Show who the user was previously invited by

--- src/commands/mod-fake.ts
+++ src/commands/mod-fake.ts
@@ -13,7 +13,7 @@ export default class extends Command<Client> {
       aliases: ['fakes', 'cheaters', 'cheater', 'invalid'],
       desc: 'Show which person joined this server multiple times and by whom he was invited',
       usage: '<prefix>fake',
-      // callerPermissions: ['ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_ROLES'],
+      callerPermissions: ['ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_ROLES'],
       clientPermissions: ['MANAGE_GUILD'],
       guildOnly: true
     });
@@ -22,59 +22,51 @@ export default class extends Command<Client> {
   public async action(message: Message, args: string[]): Promise<any> {
     this._logger.log(`${message.guild.name} (${message.author.username}): ${message.content}`);
 
-    const js = await joins.findAll({
-      attributes: [
-        'exactMatch',
-        'possibleMatches',
-        [sequelize.fn('count', sequelize.col('join.id')), 'totalJoins']
-      ],
-      where: {
-        guildId: message.guild.id,
-      },
-      group: ['join.memberId'],
-      include: [
-        members,
-        { model: inviteCodes, where: { exactMatch: 'code' }, required: false },
-      ],
-    });
-
-    console.log(js.map(j => j.get({ plain: true })));
+    const js: any[] = await sequelize.query(`
+      SELECT
+        code,
+        exactMatch,
+        possibleMatches,
+        count(j.id) AS totalJoins,
+        j.memberId,
+        m.name as memberName,
+        GROUP_CONCAT(m2.id SEPARATOR ',') AS inviterIds,
+        GROUP_CONCAT(m2.name SEPARATOR ',') AS inviterNames
+      FROM joins j
+      LEFT JOIN members m on m.id = j.memberId
+      LEFT JOIN inviteCodes ic ON ic.code = j.exactMatch
+      LEFT JOIN members m2 ON m2.id = ic.inviterId
+      WHERE j.guildId = ?
+      GROUP BY j.memberId
+    `, { replacements: [message.guild.id], type: sequelize.QueryTypes.SELECT });
 
     if (js.length > 0) {
-      const suspiciousMembers = js
-        .filter(j => parseInt(j.get('totalJoins')) > 1)
-        .sort((a, b) => a.get('totalJoins') - b.get('totalJoins'));
+      const suspiciousJoins = js
+        .filter(j => j.totalJoins > 1)
+        .sort((a, b) => a.totalJoins - b.totalJoins);
 
       const embed = new RichEmbed();
       embed.setTitle('Fake invites:');
       let description = '';
-      let guildWithAllMembers = await message.guild.fetchMembers();
 
-      /*for (let m of suspiciousMembers) {
-        let members = await getMembersByInviteCodes(guildWithAllMembers, m.invitedBy);
-        let membersInvitedStrings: string[] = [];
-        let occurances = members.reduce((a: any, b) => {
-          if (a[b.user.username]) {
-            a[b.user.username]++;
+      for (let join of suspiciousJoins) {
+        const invs: { [x: string]: number } = {};
+        join.inviterIds.split(',').forEach((id: string) => {
+          if (invs[id]) {
+            invs[id]++;
           } else {
-            a[b.user.username] = 1;
+            invs[id] = 1;
           }
-          return a;
-        }, {});
-        Object.keys(occurances).forEach(o => {
-          membersInvitedStrings.push(`${o} (${occurances[o]} times)`);
         });
-        let member = message.guild.members.get(m.userID);
-        if (member) {
-          description += `**${member.user.username}** invited by: ${membersInvitedStrings.join(', ')}\n`;
-        }
+        const invText = Object.keys(invs).map(id => `<@${id}> (**${invs[id]} times**)`).join(', ');
+        description += `<@${join.memberId}> joined **${join.totalJoins} times**, invited by: ${invText}\n`;
       }
-      if (suspiciousMembers.length === 0) {
+      if (suspiciousJoins.length === 0) {
         description = 'There have been no fake invites since the bot has been added to this server.';
       }
       embed.setDescription(description);
       createEmbed(message.client, embed);
-      message.channel.send({ embed });*/
+      message.channel.send({ embed });
     } else {
       message.channel.send(`No fake invites detected so far.`);
     }
