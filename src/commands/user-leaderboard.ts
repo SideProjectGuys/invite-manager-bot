7df872b2d8974e2d5bf1efeaa8408d524dbde1b6
Andreas Gassmann <andreas@andreasgassmann.ch>
2018-04-02 14:34:37 -0500

    feat(leaderboard): show change in last 24h (WIP)

--- src/commands/user-leaderboard.ts
+++ src/commands/user-leaderboard.ts
@@ -1,7 +1,8 @@
-import { Command, CommandDecorators, Message, Middleware, Logger, logger } from 'yamdbf';
+import { Command, CommandDecorators, Message, Middleware, Logger, logger, KeyedStorage } from 'yamdbf';
 import { Channel, RichEmbed } from 'discord.js';
 import { createEmbed } from '../utils/util';
-import { inviteCodes, sequelize, members, customInvites } from '../sequelize';
+import { Op } from 'sequelize';
+import { joins, inviteCodes, sequelize, members, customInvites } from '../sequelize';
 import { IMClient } from '../client';
 const { resolve } = Middleware;
 const { using } = CommandDecorators;
@@ -51,12 +52,14 @@ export default class extends Command<IMClient> {
       include: [members]
     });
 
-    const invs: { [x: string]: { name: string, code: number, bonus: number } } = {};
+    const invs: { [x: string]: { name: string, code: number, bonus: number, last24hours: number, position24hoursAgo: number } } = {};
     codeInvs.forEach(inv => {
       invs[inv.inviter.id] = {
         name: inv.inviter.name,
         code: parseInt(inv.get('totalUses')),
         bonus: 0,
+        last24hours: 0,
+        position24hoursAgo: 0
       };
     });
     customInvs.forEach(inv => {
@@ -67,16 +70,43 @@ export default class extends Command<IMClient> {
           name: inv.member.name,
           code: 0,
           bonus: parseInt(inv.get('totalAmount')),
+          last24hours: 0,
+          position24hoursAgo: 0
         };
       }
     });
+
+    const js = await joins.findAll({
+      attributes: [
+        [sequelize.fn('COUNT', sequelize.col('join.id')), 'totalJoins']
+      ],
+      where: {
+        ...where,
+        createdAt: {
+          [Op.gt]: new Date(new Date().getTime() - (24 * 60 * 60 * 1000))
+        }
+      },
+      group: ['join.memberId'],
+      include: [{
+        model: inviteCodes,
+        as: 'exactMatch',
+        include: [{ model: members, as: 'inviter' }]
+      }],
+    });
+
+    js.forEach(j => {
+      invs[j.exactMatch.inviter.id].last24hours = j.get('totalJoins');
+    });
+
     const keys = Object.keys(invs)
       .filter(k => invs[k].bonus + invs[k].code > 0)
       .sort((a, b) => (invs[b].code + invs[b].bonus) - (invs[a].code + invs[a].bonus));
 
+    const leaderboard24hAgo = keys.sort((a, b) => (invs[b].code + invs[b].bonus - invs[b].last24hours) - (invs[a].code + invs[a].bonus - invs[a].last24hours));
+
     let str = `Leaderboard ${channel ? 'for channel <#' + channel.id + '>' : ''}\n\n`;
 
-    console.log(invs);
+    // TODO: Calculate positions
 
     // TODO: Compare to 1 day ago
     // let upSymbol = '🔺';
