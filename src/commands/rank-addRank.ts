5dffbbcd7e2a53dcbf1db7e754b5ae78b699669f
Marco Crespi <mrc@flxlabs.com>
2018-04-07 22:43:18 +0200

    fix(add-rank): Fix not adding role to db

--- src/commands/rank-addRank.ts
+++ src/commands/rank-addRank.ts
@@ -2,7 +2,7 @@ import { Role } from 'discord.js';
 import { Command, CommandDecorators, Logger, logger, Message, Middleware } from 'yamdbf';
 
 import { IMClient } from '../client';
-import { ranks } from '../sequelize';
+import { ranks, roles } from '../sequelize';
 
 const { resolve, expect } = Middleware;
 const { using } = CommandDecorators;
@@ -27,6 +27,14 @@ export default class extends Command<IMClient> {
 	@using(expect('role: Role, invites: Number'))
 	public async action(message: Message, [role, invites, description]: [Role, number, string]): Promise<any> {
 		this._logger.log(`${message.guild.name} (${message.author.username}): ${message.content}`);
+
+		await roles.insertOrUpdate({
+			id: role.id,
+			name: role.name,
+			guildId: role.guild.id,
+			color: role.hexColor,
+			createdAt: role.createdAt
+		});
 
 		await ranks.insertOrUpdate({
 			id: null,
