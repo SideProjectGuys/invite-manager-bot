5e158e7b80d509ea2ee2fd86859a7b8fc8697bf0
Marco (Valandur) <inithilian@gmail.com>
2018-06-11 15:09:07 +0200

    Revert "Merge branch 'hotfix/backport-fixes' into develop"
    This reverts commit a79fcf733569c7c4b5d9064ccfc01600d4a21fa9.

--- src/commands/user-botInfo.ts
+++ src/commands/user-botInfo.ts
@@ -1,25 +1,66 @@
+import {
+	Command,
+	CommandDecorators,
+	Logger,
+	logger,
+	Message
+} from '@yamdbf/core';
+import moment from 'moment';
 
-			embed.addField(rp.BOT_PATREON_TITLE(), config.botPatreon);
-		}
+import { IMClient } from '../client';
+import { SettingsCache } from '../utils/SettingsCache';
+import { CommandGroup, createEmbed, RP, sendEmbed } from '../utils/util';
 
-		sendEmbed(message.channel, embed, message.author);
+const { localizable } = CommandDecorators;
+
+const config = require('../../config.json');
+
+export default class extends Command<IMClient> {
+	@logger('Command') private readonly _logger: Logger;
+
+	public constructor() {
+		super({
+			name: 'bot-info',
+			aliases: ['botInfo'],
+			desc: 'Show info about the bot',
+			usage: '<prefix>botInfo',
+			group: CommandGroup.Other
+		});
 	}
-}
 
-			embed.addField(rp.BOT_PATREON_TITLE(), config.botPatreon);
-		}
+	@localizable
+	public async action(message: Message, [rp]: [RP]): Promise<any> {
+		this._logger.log(
+			`${message.guild.name} (${message.author.username}): ${message.content}`
+		);
 
-		sendEmbed(message.channel, embed, message.author);
-	}
-}
+		const lang = (await SettingsCache.get(message.guild.id)).lang;
 
-			embed.addField(rp.BOT_PATREON_TITLE(), config.botPatreon);
-		}
+		const numGuilds = await this.client.getGuildsCount();
+		const numMembers = await this.client.getMembersCount();
 
-		sendEmbed(message.channel, embed, message.author);
-	}
-}
+		const embed = createEmbed(this.client);
 
+		// Version
+		embed.addField(rp.CMD_BOTINFO_VERSION(), this.client.version, true);
+
+		// Uptime
+		embed.addField(
+			rp.CMD_BOTINFO_UPTIME(),
+			moment
+				.duration(moment().diff(this.client.startedAt))
+				.locale(lang)
+				.humanize(),
+			true
+		);
+
+		// Guild count
+		embed.addField(rp.CMD_BOTINFO_GUILDS(), numGuilds, true);
+
+		// Member count
+		embed.addField(rp.CMD_BOTINFO_MEMBERS(), numMembers, true);
+
+		// Shard info
 		embed.addField(
 			rp.CMD_BOTINFO_SHARD_CURRENT(),
 			this.client.options.shardId + 1,
@@ -30,6 +71,21 @@
 			this.client.options.shardCount,
 			true
 		);
+
+		// Support discord
+		if (config.botSupport) {
+			embed.addField(rp.BOT_SUPPORT_DISCORD_TITLE(), config.botSupport);
+		}
+		// Add bot
+		if (config.botAdd) {
+			embed.addField(rp.BOT_INVITE_TITLE(), config.botAdd);
+		}
+		// Bot website
+		if (config.botWebsite) {
+			embed.addField(rp.BOT_WEBSITE_TITLE(), config.botWebsite);
+		}
+		// Patreon
+		if (config.botPatreon) {
 			embed.addField(rp.BOT_PATREON_TITLE(), config.botPatreon);
 		}
 
