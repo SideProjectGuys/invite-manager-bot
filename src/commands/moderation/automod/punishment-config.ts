693533efd3e4581d6aa1eb09e8f17599f1385cb3
AndreasGassmann <andreas@andreasgassmann.ch>
2018-08-31 00:50:35 +0200

    feat(punishmentconfig): allow removal and show unused

--- src/commands/moderation/automod/punishment-config.ts
+++ src/commands/moderation/automod/punishment-config.ts
@@ -19,8 +19,7 @@ export default class extends Command {
 			args: [
 				{
 					name: 'punishment',
-					resolver: new EnumResolver(client, Object.values(PunishmentType)),
-					required: true
+					resolver: new EnumResolver(client, Object.values(PunishmentType))
 				},
 				{
 					name: 'strikes',
@@ -50,28 +49,46 @@ export default class extends Command {
 			title: t('cmd.punishmentConfig.title')
 		});
 
-		if (typeof strikes !== typeof undefined) {
+		let punishmentQuery = {
+			guildId: guild.id,
+			punishmentType: punishment
+		};
+		if (typeof punishment === typeof undefined) {
+			let allPunishments: PunishmentType[] = Object.values(PunishmentType);
+			let punishmentConfigList =
+				await punishmentConfigs.findAll({ where: { guildId: guild.id }, order: [['amount', 'DESC']] });
+			let unusedPunishment =
+				allPunishments.filter(p => punishmentConfigList.map(pcl => pcl.punishmentType).indexOf(p) < 0);
+			embed.description =
+				punishmentConfigList.map(pcl => t('cmd.punishmentConfig.text', {
+					punishment: `**${pcl.punishmentType}**`,
+					strikes: `**${pcl.amount}**`
+				})).join('\n');
+			embed.fields.push({
+				name: t('cmd.punishmentConfig.unusedPunishment'),
+				value: `\n${unusedPunishment.map(v => `\`${v}\``).join(', ')}`
+			});
+		} else if (typeof strikes === typeof undefined) {
+			const pc = await punishmentConfigs.find({ where: punishmentQuery });
+			embed.description = t('cmd.punishmentConfig.text', {
+				punishment: `**${pc.punishmentType}**`,
+				strikes: `**${pc.amount}**`
+			});
+		} else if (strikes === 0) {
+			await punishmentConfigs.destroy({ where: punishmentQuery });
+			embed.description = t('cmd.punishmentConfig.deletedText', {
+				punishment: `**${punishment}**`
+			});
+		} else {
 			punishmentConfigs.insertOrUpdate({
 				id: null,
-				guildId: guild.id,
-				punishmentType: punishment,
 				amount: strikes,
-				args: args
+				args: args,
+				...punishmentQuery
 			});
 			embed.description = t('cmd.punishmentConfig.text', {
 				punishment: `**${punishment}**`,
 				strikes: `**${strikes}**`
-			});
-		} else {
-			const pc = await punishmentConfigs.find({
-				where: {
-					guildId: guild.id,
-					punishmentType: punishment
-				}
-			});
-			embed.description = t('cmd.punishmentConfig.text', {
-				punishment: `**${pc.punishmentType}**`,
-				strikes: `**${pc.amount}**`
 			});
 		}
 
