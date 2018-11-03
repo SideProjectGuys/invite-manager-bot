f7b33ae4035ccfd1c96406681a6a9fb63201a0b3
Marco Crespi <mrc@flxlabs.com>
2018-11-03 17:08:34 +0100

    fix(mention): Check roles before mentioning

--- src/commands/roles/makeMentionable.ts
+++ src/commands/roles/makeMentionable.ts
@@ -26,16 +26,32 @@ export default class extends Command {
 	public async action(
 		message: Message,
 		[role]: [Role],
-		{ t }: Context
+		{ t, me, guild }: Context
 	): Promise<any> {
-		await message.delete();
-
 		if (role.mentionable) {
 			return this.client.sendReply(
 				message,
 				t('cmd.makeMentionable.alreadyDone', { role: `<@&${role.id}>` })
 			);
 		} else {
+			let myRole: Role;
+			me.roles.forEach(r => {
+				const gRole = guild.roles.get(r);
+				if (!myRole || gRole.position > myRole.position) {
+					myRole = gRole;
+				}
+			});
+			// Check if we are higher then the role we want to edit
+			if (myRole.position < role.position) {
+				return this.client.sendReply(
+					message,
+					t('cmd.mentionRole.roleTooHigh', {
+						role: role.name,
+						myRole: myRole.name
+					})
+				);
+			}
+
 			await role.edit({ mentionable: true }, 'Pinging role');
 
 			const func = async (msg: Message) => {
@@ -55,6 +71,8 @@ export default class extends Command {
 			};
 
 			setTimeout(timeOut, 60000);
+
+			await message.delete();
 		}
 	}
 }
