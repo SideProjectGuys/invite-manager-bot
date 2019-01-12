5778fed98b4d97531b0d759d3c5e8f13540001b3
Marco (Valandur) <inithilian@gmail.com>
2019-01-12 15:35:22 +0100

    fix(subtract): Fix subtract fakes and leaves commands

--- src/commands/invites/subtractLeaves.ts
+++ src/commands/invites/subtractLeaves.ts
@@ -1,13 +1,7 @@
 import { Message } from 'eris';
 
 import { IMClient } from '../../client';
-import {
-	customInvites,
-	inviteCodes,
-	joins,
-	leaves,
-	sequelize
-} from '../../sequelize';
+import { sequelize } from '../../sequelize';
 import { BotCommand, CommandGroup } from '../../types';
 import { Command, Context } from '../Command';
 
@@ -28,79 +22,18 @@ export default class extends Command {
 		flags: {},
 		{ guild, t, settings }: Context
 	): Promise<any> {
-		const ls = await leaves.findAll({
-			attributes: [
-				'memberId',
-				[
-					sequelize.fn(
-						'TIMESTAMPDIFF',
-						sequelize.literal('SECOND'),
-						sequelize.fn('MAX', sequelize.col('join.createdAt')),
-						sequelize.fn('MAX', sequelize.col('leave.createdAt'))
-					),
-					'timeDiff'
-				]
-			],
-			where: {
-				guildId: guild.id
-			},
-			group: [
-				sequelize.col('leave.memberId'),
-				sequelize.col('join->exactMatch.code')
-			],
-			include: [
-				{
-					attributes: [],
-					model: joins,
-					required: true,
-					include: [
-						{
-							attributes: ['code', 'inviterId'],
-							model: inviteCodes,
-							as: 'exactMatch',
-							required: true
-						}
-					]
+		await sequelize.query(
+			`UPDATE joins j LEFT JOIN leaves l ON l.joinId = j.id SET invalidatedReason = ` +
+				`CASE WHEN l.id IS NULL OR TIMESTAMPDIFF(MINUTE, j.createdAt, l.createdAt) > :time THEN NULL ELSE 'leave' END ` +
+				`WHERE j.guildId = :guildId AND j.invalidatedReason IS NULL`,
+			{
+				replacements: {
+					guildId: guild.id,
+					time: settings.autoSubtractLeaveThreshold
 				}
-			],
-			raw: true
-		});
-
-		if (ls.length === 0) {
-			return this.client.sendReply(message, t('cmd.subtractLeaves.none'));
-		}
-
-		/*
-		// Delete old duplicate removals
-		await customInvites.destroy({
-			where: {
-				guildId: guild.id,
-				generatedReason: CustomInvitesGeneratedReason.leave
 			}
-		});
-
-		const threshold = settings.autoSubtractLeaveThreshold;
-
-		// Add subtracts for leaves
-		const customInvs = ls
-			.filter((l: any) => parseInt(l.timeDiff, 10) < threshold)
-			.map((l: any) => ({
-				id: null,
-				guildId: guild.id,
-				memberId: l['join.exactMatch.inviterId'],
-				creatorId: null,
-				amount: -1,
-				reason: l.memberId,
-				generatedReason: CustomInvitesGeneratedReason.leave
-			}));
-		await customInvites.bulkCreate(customInvs, {
-			updateOnDuplicate: ['amount', 'updatedAt']
-		});
-
-		return this.client.sendReply(
-			message,
-			t('cmd.subtractLeaves.done', { total: customInvs.length })
 		);
-		*/
+
+		return this.client.sendReply(message, t('cmd.subtractLeaves.done'));
 	}
 }
