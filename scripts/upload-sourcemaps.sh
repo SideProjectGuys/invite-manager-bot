a6d32fccbf91fa5dad1b3cec63b8b7df3dec67c4
Marco Crespi <mrc@flxlabs.com>
2019-09-11 09:44:21 +0200

    chore(sentry): Pull git changes

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -2,12 +2,15 @@
 set -e
 
 if [ ! -f "node_modules/.bin/sentry-cli" ]; then
-	echo "Installing local sentry..."
+	echo "Installing local sentry"
 	npm install @sentry/cli
 fi
 
 echo "Switching to master"
 git checkout master
+
+echo "Pulling newest commit"
+git pull
 
 echo "Running build"
 npm run build
