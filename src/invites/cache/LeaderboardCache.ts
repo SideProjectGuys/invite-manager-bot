d1e6675acf7b5c4f7e49f4cee3b80a79bb9115ee
Marco Crespi <inithilian@gmail.com>
2019-11-19 12:20:11 +0100

    fix(leaderboard): Adjust cache duration
    Shorter cache time for custom
    Longer cache time for regular

--- src/invites/cache/LeaderboardCache.ts
+++ src/invites/cache/LeaderboardCache.ts
@@ -1,11 +1,17 @@
 import moment from 'moment';
 
 import { Cache } from '../../framework/cache/Cache';
+import { BotType } from '../../types';
 import { LeaderboardEntry } from '../services/Invites';
 
 export class LeaderboardCache extends Cache<LeaderboardEntry[]> {
 	public async init() {
-		this.maxCacheDuration = moment.duration(1, 'hour');
+		this.maxCacheDuration =
+			this.client.type === BotType.custom
+				? moment.duration(5, 'minute')
+				: this.client.type === BotType.pro
+				? moment.duration(1, 'hour')
+				: moment.duration(24, 'hour');
 	}
 
 	protected async _get(guildId: string): Promise<LeaderboardEntry[]> {
