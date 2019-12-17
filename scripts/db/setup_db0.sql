5891cd366b65f805f690dcf222260cef8d4263f6
Marco Crespi <inithilian@gmail.com>
2019-12-17 17:19:26 +0100

    chore(setup): Reduce dbstats key size

--- scripts/db/setup_db0.sql
+++ scripts/db/setup_db0.sql
@@ -31,7 +31,7 @@ CREATE TABLE `botSettings` (
 --
 
 CREATE TABLE `dbStats` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `key` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` double DEFAULT NULL,
   `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
